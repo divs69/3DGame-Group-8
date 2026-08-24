e6cac3b53c14849aff8fc89402bdc2800fbf53b2
nashy-boop <nashtikakhandai@gmail.com>
2026-08-24 20:41:10 +0200

    Update
    I added the  bone mesh and material, i placed it by culdren, so you can see how it looks, its not set up as a collectable. i added my Potion making Ui, when the player enters the area a button will pop up to open the culdren panel. I cant test coz player not moving. all buttons work on a prephab tho so its just about setting up correctly

---
+++ 3D Game Group 8 Sem 2/Assets/Scripts/CuldrenButtonPopUp.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class CuldrenButtonPopup : MonoBehaviour
+{
+    //ui button
+    [SerializeField] private GameObject uiButton;
+
+    //playerTag
+    [SerializeField] private string targetTag = "Player";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        //hidden button
+        if(uiButton != null)
+        {
+            uiButton.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //object entering is plaayer check
+        if (other.CompareTag(targetTag))
+        {
+            if (uiButton != null)
+            {
+              uiButton.SetActive(true);
+            }
+        }     
+    }
+
+
+    // Update is called once per frame
+    private void OnTriggerExit(Collider other)
+    {
+        //Hide button after
+        if (other.CompareTag(targetTag))
+        {
+          if (uiButton !=null)
+            {
+                uiButton.SetActive(false);
+            }
+        }
+    }
+}
